b1bb26e52e64c1c1e832fcbaa76ea16b25fde154
Tomasz <tomasz.cichoszewski.x@gmail.com>
2015-02-07 20:37:29 +0100

    dodane pobieranie bazy

--- exchange/views.py
+++ exchange/views.py
@@ -1,7 +1,11 @@
+from os.path import os
 from django.http import HttpResponse
+from django.core.servers.basehttp import FileWrapper
 from django.http import JsonResponse
-from exchange.models import Version
+
 from db.settings import DATABASES
+from exchange.models import Version
+
 
 def index(request):
     version = Version.objects.first()
@@ -10,5 +14,9 @@ def index(request):
     return JsonResponse({'dbVersion': str(version)})
 
 def download(request):
-    response = HttpResponse(DATABASES['name'])
+    path = DATABASES['default']['NAME']
+    wrapper = FileWrapper(open(path, 'rb'))
+    response = HttpResponse(wrapper, content_type='text/plain')
+    response['Content-Length'] = os.path.getsize(path)
+    response['Content-Disposition'] = 'attachment; filename=poligdzie.db'
     return response
