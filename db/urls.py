a7f9d3cba6b68f99c92218f41c21a24616e33a20
Tomasz <tomasz.cichoszewski.x@gmail.com>
2015-02-07 18:10:45 +0100

    rozdzielony url do pobierania bazy

--- db/urls.py
+++ db/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from exchange.views import *
+import exchange
 
 urlpatterns = patterns('',
-    url(r'^version/', include('exchange.urls')),
+    url(r'^version/', exchange.views.index),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^download/', exchange.views.download),
 )
--- exchange/views.py
+++ exchange/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
-from db.settings import DB_VERSION
+from django.http import JsonResponse
 from exchange.models import Version
+from db.settings import DATABASES
 
 def index(request):
     version = Version.objects.first()
     if not version:
         version = Version.objects.create(value=1)
-    
-    
-    return HttpResponse("{\n\t\"dbVersion\" : " + str(version) + "\n}")
+    return JsonResponse({'dbVersion': str(version)})
+
+def download(request):
+    response = HttpResponse(DATABASES['name'])
+    return response
